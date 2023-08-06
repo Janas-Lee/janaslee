92101e051ed60460b01208d9ab36157228aa4763
JanasLee <janaslee@gmail.com>
2023-08-06 18:23:13 +0800

    add img directory

---
+++ php/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="zh-TW">
+<head>
+</head>
+<body>
+ <?php  echo "My first PHP script!"; ?><br> 
+ <a href="https://qnaire.ab1.auo.com/zh-hant/node/3949">AUO 3949</a><br>
+ <a href="https://qnaire.ab1.auo.com/zh-hant/node/4074">AUO 4074 門口 763592</a><br>
+
+</body>
+</html>
